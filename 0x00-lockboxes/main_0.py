3bcf690d4d29cbc2ddd6a960f43cbfca36593939
apla02 <apla02@gmail.com>
2021-12-12 19:01:14 -0500

    style

--- 0x00-lockboxes/main_0.py
+++ 0x00-lockboxes/main_0.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 
 canUnlockAll = __import__('0-lockboxes').canUnlockAll
@@ -11,4 +10,3 @@ print(canUnlockAll(boxes))
 
 boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
 print(canUnlockAll(boxes))
-
