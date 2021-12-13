cab974ad0becf2c0e1b9fac5eb97017fa0fcc1e7
apla02 <apla02@gmail.com>
2021-12-12 19:00:31 -0500

    readme

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-# #Solving algorithm
+
 
 def canUnlockAll(boxes):
     '''
     You have n number of locked boxes in front of you.
-    Each box is numbered sequentially from 0 to n - 1 and each box may contain keys to the other boxes.
+    Each box is numbered sequentially from 0 to n - 1 and each
+    box may contain keys to the other boxes.
     Write a method that determines if all the boxes can be opened.
     '''
-    
 
     if boxes is None or len(boxes) is 0:
         return False
@@ -30,7 +30,3 @@ def canUnlockAll(boxes):
     if "F" in status:
         return False
     return True
-
-
-
-
